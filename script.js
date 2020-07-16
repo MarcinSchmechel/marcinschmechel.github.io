4dd7b44076babfce33173afa0bb7ed2cf1c4df7f
MarcinSchmechel <marcin.schmechel@o2.pl>
2020-07-16 17:44:58 +0200

    Frontend with features2

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://afternoon-cove-07276.herokuapp.com/v1/task/';
-  // const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
-  // const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://afternoon-cove-07276.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
